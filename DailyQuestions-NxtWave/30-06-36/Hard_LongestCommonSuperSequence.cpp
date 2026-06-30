d5bfcd16a19dd813a19b071af465fbc5f89dc89b
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-30 17:22:32 +0530

    Create Hard_LongestCommonSuperSequence.cpp

---
+++ DailyQuestions-NxtWave/30-06-36/Hard_LongestCommonSuperSequence.cpp
@@ -0,0 +1,75 @@
+/*
+
+Approach
+
+***************Refer My notes****************
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution
+{
+public:
+    string shortestCommonSupersequence(string s1, string s2)
+    {
+        // Write your code here...
+        int m = s1.size();
+        int n = s2.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int i = m;
+        int j = n;
+        string ans = "";
+        while (i > 0 && j > 0)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                ans.push_back(s1[i - 1]);
+                i--;
+                j--;
+            }
+            else if (dp[i - 1][j] >= dp[i][j - 1])
+            {
+                ans.push_back(s1[i - 1]);
+                i--;
+            }
+            else
+            {
+                ans.push_back(s2[j - 1]);
+                j--;
+            }
+        }
+
+        while (i > 0)
+        {
+            ans.push_back(s1[i - 1]);
+            i--;
+        }
+        while (j > 0)
+        {
+            ans.push_back(s2[j - 1]);
+            j--;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
