e5e2079a900681b67e3e59e3fb80c253de20b333
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-07-02 18:45:44 +0530

    Create Spiral-Matrix.cpp

---
+++ NxtWave/Arrays-NxtWave/Medium/Spiral-Matrix.cpp
@@ -0,0 +1,9 @@
+/*
+
+Approach
+
+1)we first store the directions like dx and dy so when ever we wan tot change the pos ..we do pos++ and add dx[pos],dy[pos] to row and col numbers
+2)we stop printing the numbers when the no of pronter numbers become n2
+3)at each iterationn we first check tht nextrow,nextcol is valid or not then only go to next row and next col
+
+*/
