93228704061f8fcd4ed30d9e013f42c7f9bba71c
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-07-04 17:59:23 +0530

    Update Symmetric-Binary-tree.cpp

--- NxtWave/BinaryTrees-NxtWave/Medium/Symmetric-Binary-tree.cpp
+++ NxtWave/BinaryTrees-NxtWave/Medium/Symmetric-Binary-tree.cpp
@@ -7,3 +7,44 @@ Approach
 3)to be symetric first their node values sholuld eb equal and ones left = another right,ones right = another left
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data) : data(data), left(nullptr), right(nullptr) {}
+};
+
+class solution
+{
+public:
+    bool check(Node *root1, Node *root2)
+    {
+        if (!root1 && !root2)
+            return true;
+        if (!root1 && root2)
+            return false;
+        if (root1 && !root2)
+            return false;
+
+        if (root1->data != root2->data)
+        {
+            return false;
+        }
+        return check(root1->left, root2->right) && check(root1->right, root2->left);
+    }
+
+    bool symmetric(Node *root)
+    {
+        // Write your code here...
+        if (root == NULL)
+            return true;
+        return check(root->left, root->right);
+    }
+};
