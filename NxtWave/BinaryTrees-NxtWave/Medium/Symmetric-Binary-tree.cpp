5a94a307835fa66e53f21bf635bbf9495cf09c10
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-07-04 17:59:01 +0530

    Create Symmetric-Binary-tree.cpp

---
+++ NxtWave/BinaryTrees-NxtWave/Medium/Symmetric-Binary-tree.cpp
@@ -0,0 +1,9 @@
+/*
+
+Approach
+
+1)for a tree to be symmetric -- its right and left subtrees should be symmetric
+2)so lets create another function which takes 2 nodes root1 and root2 and recursively check the symmetry
+3)to be symetric first their node values sholuld eb equal and ones left = another right,ones right = another left
+
+*/
